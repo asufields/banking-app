6d8b5dd25c3d801f4e445ff4620d8b9455dbd865
Ashley Fields <asufields@gmail.com>
2024-10-16 14:27:15 -0400

    implement initial file and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+export const dynamic = 'force-dynamic'
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        {children}
+    
+    </main>
+  );
+}
